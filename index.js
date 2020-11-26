d5143d33c63611c39bee78f82e4e481ebae528da
plebtech <nbmc@protonmail.com>
2020-11-26 16:36:30 -0600

    bot testing function, segregated and secured token key.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-console.log("loaded.");
+// load config json.
+const config = require('./config.json');
+// require discord.js module.
+const Discord = require('discord.js');
+// instantiate client from discord.js.
+const client = new Discord.Client();
+
+
+// when client ready, run once.
+client.once('ready', () => {
+    console.log('ready.');
+});
+
+client.on('message', message => {
+    if (message.content === 'kek') {
+        message.channel.send('KEK');
+    }
+});
+
+// log in to discord with app token.
+client.login(config.token);
 
 // approximating square root via Heron of Alexandria's formula.
-const heronRoot = square => {
+function heronRoot(square) {
     let guess = Math.random() * square; // begin from random point between 0 and the square.
     let lastGuess = 0;
     while (guess.toFixed(4) != lastGuess.toFixed(4)) {
