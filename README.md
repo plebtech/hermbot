a825cbfa94982b8002cc33e5b690e3fe91de5d7e
plebtech <nbmc@protonmail.com>
2020-11-26 18:45:41 -0600

    added comment, updated readme.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # hermbot
 ## simple custom discord bot, discord.js library.
 
+### commands:
+heron {integer} - return the approximate square root of a given positive integer, implementing 
+
+### to do:
 first goal: get the bot to automatically convert between imperial and metric values.
--- index.js
+++ index.js
@@ -7,11 +7,12 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+    // if message is not prefixed for this bot or is sent by bot, ignore.
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
 
-    if (command === 'heron') {
+    if (command[0] === 'heron') {
         square = parseInt(args[0]);
         if (isNaN(square) || (square < 0)) {
             message.channel.send('please include a positive numeric argument.');
@@ -28,13 +29,13 @@ const fix = '\`\`\`fix\n';
 const brainfuck = '\`\`\`brainfuck\n';
 
 const heronRoot = (square, message) => {
-    let guess = Math.random() * square;
-    let lastGuess = 0;
+    let guess = parseFloat(Math.random() * square).toFixed(4);
+    let lastGuess = parseFloat(0).toFixed(4);
     let response = `${brainfuck}approximating the square root of ${square} via successive averages:\n`;
-    while (guess.toFixed(1) != lastGuess.toFixed(1)) {
+    while (guess != lastGuess) {
         response += `${guess}\n`;
         lastGuess = guess;
-        guess = (guess + (square / guess)) / 2;
+        guess = parseFloat((guess + (square / guess)) / 2).toFixed(4);
     }
     response += `best guess: ${guess} \n \`\`\``;
     message.channel.send(response);
