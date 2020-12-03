0f8f14cc326eba01bbd39e4bc1b48374f2accaaf
plebtech <nbmc@protonmail.com>
2020-12-02 23:15:08 -0600

    testing pennant module.

--- pennant.js
+++ pennant.js
@@ -3,7 +3,20 @@ const fix = '\`\`\`fix\n';
 const brainfuck = '\`\`\`brainfuck\n';
 
 const post = (args, message) => {
-    message.channel.send(args);
+    input = '';
+    output = `${css}`;
+    for (i = 0; i < args.length; i++) {
+        input = input + args[i];
+    }
+    for (i = 0; i < input.length; i++) {
+        if (input.charAt(i) === ' ') {
+            continue;
+        }
+        let thisWord = input.slice(i, input.length);
+        output += thisWord.split('').join(' ').toUpperCase());
+    }
+    output += '\`\`\`';
+    message.channel.send(output);
 }
 
 // for (i = 0; i < word.length; i++) {
