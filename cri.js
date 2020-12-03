b63449171c83dd35053032e889f8a65c13fcf9a3
plebtech <nbmc@protonmail.com>
2020-12-02 22:11:48 -0600

    fixed typo in cri module.

--- cri.js
+++ cri.js
@@ -2,7 +2,7 @@ const square = (word, message) => {
     let output = '';
     for (i = 0; i < word.length; i++) {
         for (j = 0; j < word.length; j++) {
-            output = pout + word[i] + word[j];
+            output = output + word[i] + word[j];
         }
     }
     message.channel.send(output);
