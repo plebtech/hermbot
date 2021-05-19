1703cf3f7c6e51650b37696049fe9ce15b44e19e
plebtech <nbmc@protonmail.com>
2021-05-19 10:36:23 -0500

    changed sup module to reply inline.

--- sup.js
+++ sup.js
@@ -6,7 +6,7 @@ let timeout = false;
 
 const supWatch = async (message) => {
     if (message.content === ('sup') && (timeout === false)) {
-        message.channel.send('nm u');
+        message.reply('nm u');
         timeout = true;
         await timer(supDelay);
         timeout = false;
