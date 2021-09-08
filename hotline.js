e38c153090b25838750ba419dafeb29c118c816b
plebtech <nbmc@protonmail.com>
2021-09-08 12:11:19 -0500

    added message deletion to hotline.js.

--- hotline.js
+++ hotline.js
@@ -6,12 +6,15 @@ const timer = ms => new Promise(res => setTimeout(res, ms));
 let timeout = false;
 
 const hotlineWatch = async (message) => {
-    if  ((timeout === false) &&
+    if ((timeout === false) &&
         ((message.content.toLowerCase().includes('suicid') ||
-        (message.author.id === '261942742525935628')))) {
+            (message.author.id === '261942742525935628')))) {
         timeout = true;
         await timer(randomNumber.generate(25, 750));
-        message.lineReply('the national suicide prevention lifeline is: \`1-800-273-8255\`');
+        message.lineReply('the national suicide prevention lifeline is: \`1-800-273-8255\`')
+            .then(msg => {
+                msg.delete({ timeout: 60000 })
+            });
         await timer(300000);
         timeout = false;
     }
