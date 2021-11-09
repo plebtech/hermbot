1750a26fa6014bd17315874a8068c4f5621f8051
plebtech <nbmc@protonmail.com>
2021-11-09 10:45:20 -0600

    reimplemented eightyFour.js.

--- eightyFour.js
+++ eightyFour.js
@@ -8,15 +8,14 @@ let timeout = false;
 const eightyFourWatch = async (message) => {
     if ((timeout === false) &&
         ((message.content.toLowerCase().includes('1984')))) {
-            message.lineReplyNoMention(eightyFourDelay);
-        // timeout = true;
-        // await timer(randomNumber.generate(25, 750));
-        // message.lineReplyNoMention('1984 is a great fiction novel to read but it seems like it is becoming the reality we are currently living under more and more each day.')
-        //     .then(msg => {
-        //         msg.delete({ timeout: 60000 })
-        //     });
-        // await timer(300000);
-        // timeout = false;
+        timeout = true;
+        await timer(randomNumber.generate(25, 750));
+        message.lineReplyNoMention('1984 is a great fiction novel to read but it seems like it is becoming the reality we are currently living under more and more each day.')
+            .then(msg => {
+                msg.delete({ timeout: 60000 })
+            });
+        await timer(eightyFourDelay);
+        timeout = false;
     }
     if (message.content.toLowerCase().includes('1984') ||
         (message.content.toLowerCase().includes('nineteen eighty four'))) {
