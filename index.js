5952f822e7a08068579e950eb2b74ccc69b4af2c
plebtech <nbmc@protonmail.com>
2022-04-01 07:48:38 -0500

    testing adding back bump watch.

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ client.once('ready', () => { // on ready.
 const disboardCountDown = async () => { // async function to time secondary catch for bump.
     if (disboardTimeToWait <= 0) { // if timer has reached zero, alert to bump.
         disboardSecondaryCatch = true;
-        general.send('please type `/bump`')
-            .then(msg => {
-                msg.delete({ timeout: 60000 })
-            });
+        // general.send('please type `/bump`')
+        //     .then(msg => {
+        //         msg.delete({ timeout: 60000 })
+        //     });
         return;
     } else { // count down the time to wait every minute.
         while (disboardTimeToWait > 0) {
@@ -86,10 +86,10 @@ const bumpQuery = async (message) => {
 
 const bumpNag = async (message) => {
     unbumpedNag = true;
-    general.send('please type `/bump`')
-        .then(msg => {
-            msg.delete({ timeout: 60000 })
-        });
+    // general.send('please type `/bump`')
+    //     .then(msg => {
+    //         msg.delete({ timeout: 60000 })
+    //     });
     await timer(60000);
     unbumpedNag = false;
 }
