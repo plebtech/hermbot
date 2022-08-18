8f30865b83fcf7d7ef0198df7dabb2225b64784d
plebtech <nbmc@protonmail.com>
2022-08-17 20:23:28 -0500

    added desu reply.

--- princess.js
+++ princess.js
@@ -2,7 +2,7 @@ const randomNumber = require('./randomNumber.js');
 
 // basic timer w/ promise using setTimeout function.
 const timer = ms => new Promise(res => setTimeout(res, ms));
-let timeout = false;
+let timeout, timeout2 = false;
 
 const princessWatch = async (message) => {
     if (
@@ -34,6 +34,20 @@ const princessWatch = async (message) => {
             .catch();
         await timer(300000);
         timeout = false;
+    }
+    if (
+        (timeout2 === false) &&
+        (message.content.toLowerCase().includes('desu'))
+    ) {
+        timeout2 = true;
+        await timer(randomNumber.generate(25, 750));
+        message.reply('DESU DESU DESU')
+            .then(msg => {
+                setTimeout(() => msg.delete(), (1000 * 30))
+            })
+            .catch();
+        await timer(300000);
+        timeout2 = false;
     }
 }
 
